c85c0b133ff2b345c6a5efa8232e6cd6b9a683fe
Dario Reis <guttyerresjunior1995@gmail.com>
2022-11-22 18:32:37 -0300

    adiciona rota no controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('atores')
   getAtores(): string {
     return this.appService.getAtores();
   }
